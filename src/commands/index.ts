5307848dabf36d2f8224480c24a172bea878f7d3
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-09-23 13:35:20 +0200

    chore: export commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,4 +1,8 @@
+import { Command as ManagerLog } from "./manager-log";
+import { Command as ManagerRestart } from "./manager-restart";
 import { Command as ManagerRun } from "./manager-run";
 import { Command as ManagerStart } from "./manager-start";
+import { Command as ManagerStatus } from "./manager-status";
+import { Command as ManagerStop } from "./manager-stop";
 
-export const Commands = [ManagerRun, ManagerStart];
+export const Commands = [ManagerLog, ManagerRestart, ManagerRun, ManagerStart, ManagerStatus, ManagerStop];
